0fae648e8753fe044a96f29000e904ec447295d8
Plep-m <menut34@gmail.com>
2024-01-18 13:37:11 +0100

    testing velkoz Q

--- Content/LeagueSandbox-Scripts/Characters/Velkoz/Q.cs
+++ Content/LeagueSandbox-Scripts/Characters/Velkoz/Q.cs
@@ -1,25 +1,24 @@
-using System;
-using GameServerCore.Enums;
-using GameServerCore.Domain.GameObjects;
-using LeagueSandbox.GameServer.Scripting.CSharp;
+using System.Collections.Generic;
 using System.Numerics;
+using GameServerCore.Domain.GameObjects;
 using GameServerCore.Domain.GameObjects.Spell;
 using GameServerCore.Domain.GameObjects.Spell.Missile;
+using GameServerCore.Enums;
 using LeagueSandbox.GameServer.API;
-using System.Collections.Generic;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.Scripting.CSharp;
 using GameServerCore.Scripting.CSharp;
-using GameServerCore.Domain.GameObjects.Spell.Sector;
+using System;
+
 
 namespace Spells
 {
-    public class PlasmaFisson : ISpellScript
+    public class PlasmaFission : ISpellScript
     {
+        Vector2 teleportTo;
         public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
-            TriggersSpellCasts = true,
-            IsDamagingSpell = true,
-            // TODO
+            TriggersSpellCasts = true
         };
 
         public void OnActivate(IObjAiBase owner, ISpell spell)
@@ -32,21 +31,22 @@ namespace Spells
 
         public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
         {
-            var targetPos = new Vector2(spell.CastInfo.TargetPosition.X, spell.CastInfo.TargetPosition.Z);
-            FaceDirection(targetPos, owner);
+            teleportTo = new Vector2(end.X, end.Y);
+            float targetPosDistance = Math.Abs((float)Math.Sqrt(Math.Pow(owner.Position.X - teleportTo.X, 2f) + Math.Pow(owner.Position.Y - teleportTo.Y, 2f)));
+            FaceDirection(teleportTo, owner);
+            teleportTo = GetPointFromUnit(owner, Math.Min(targetPosDistance, 400f));
         }
 
         public void OnSpellCast(ISpell spell)
         {
-
+            var owner = spell.CastInfo.Owner;
+            AddParticle(owner, null, "JackintheboxPoof2.troy", owner.Position, 2f);
         }
 
         public void OnSpellPostCast(ISpell spell)
         {
             var owner = spell.CastInfo.Owner;
-            var distance = GetPointFromUnit(owner, 1100f);
-
-            SpellCast(owner, 0, SpellSlotType.ExtraSlots, distance, distance, false, Vector2.Zero);
+            TeleportTo(spell.CastInfo.Owner, teleportTo.X, teleportTo.Y);
         }
 
         public void OnSpellChannel(ISpell spell)
@@ -56,68 +56,9 @@ namespace Spells
         public void OnSpellChannelCancel(ISpell spell, ChannelingStopSource reason)
         {
         }
+
         public void OnSpellPostChannel(ISpell spell)
         {
-
-        }
-
-        public void OnUpdate(float diff)
-        {
-        }
-    }
-    public class PlasmaFissionSpell : ISpellScript
-    {
-        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
-        {
-            TriggersSpellCasts = true,
-            MissileParameters = new MissileParameters
-            {
-                Type = MissileType.Circle
-            },
-            IsDamagingSpell = true,
-            // TODO
-        };
-
-        public void OnActivate(IObjAiBase owner, ISpell spell)
-        {
-            ApiEventManager.OnSpellHit.AddListener(this, spell, TargetExecute, false);
-        }
-
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
-        {
-        }
-
-        public void OnSpellCast(ISpell spell)
-        {
-        }
-
-        public void OnSpellPostCast(ISpell spell)
-        {
-        }
-        public void TargetExecute(ISpell spell, IAttackableUnit target, ISpellMissile missile, ISpellSector sector)
-        {
-            var owner = spell.CastInfo.Owner;
-            float ap = owner.Stats.AbilityPower.Total * 0.5f;
-            float damage = 60 + (spell.CastInfo.SpellLevel - 1) * 30 + ap;
-
-            target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
-            AddParticleTarget(owner, target, "cryo_ice_impact.troy", target, size: 0.5f);
-        }
-
-        public void OnSpellChannel(ISpell spell)
-        {
-        }
-
-        public void OnSpellChannelCancel(ISpell spell, ChannelingStopSource reason)
-        {
-        }
-        public void OnSpellPostChannel(ISpell spell)
-        {
-
         }
 
         public void OnUpdate(float diff)
