b17609e18a2a83f38181a461a2f2711ba857f4cc
Plep-m <menut34@gmail.com>
2024-01-18 17:26:40 +0100

    testing velkoz Q

--- Content/LeagueSandbox-Scripts/Characters/Velkoz/Q.cs
+++ Content/LeagueSandbox-Scripts/Characters/Velkoz/Q.cs
@@ -18,17 +18,12 @@ namespace Spells
         public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
             TriggersSpellCasts = true,
-            MissileParameters = new MissileParameters
-            {
-                Type = MissileType.Circle
-            },
             IsDamagingSpell = true,
+            // TODO
         };
-
 
         public void OnActivate(IObjAiBase owner, ISpell spell)
         {
-            ApiEventManager.OnSpellHit.AddListener(this, spell, TargetExecute, false);
         }
 
         public void OnDeactivate(IObjAiBase owner, ISpell spell)
@@ -43,8 +38,7 @@ namespace Spells
 
         public void OnSpellCast(ISpell spell)
         {
-            LogDebug("VelkozQ cast");
-            
+
         }
 
         public void OnSpellPostCast(ISpell spell)
@@ -55,28 +49,75 @@ namespace Spells
             SpellCast(owner, 0, SpellSlotType.ExtraSlots, distance, distance, false, Vector2.Zero);
         }
 
+        public void OnSpellChannel(ISpell spell)
+        {
+        }
+
+        public void OnSpellChannelCancel(ISpell spell, ChannelingStopSource reason)
+        {
+        }
+        public void OnSpellPostChannel(ISpell spell)
+        {
+
+        }
+
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+    public class VelkozQSpell : ISpellScript
+    {
+        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            TriggersSpellCasts = true,
+            MissileParameters = new MissileParameters
+            {
+                Type = MissileType.Circle
+            },
+            IsDamagingSpell = true,
+            // TODO
+        };
+
+        public void OnActivate(IObjAiBase owner, ISpell spell)
+        {
+            ApiEventManager.OnSpellHit.AddListener(this, spell, TargetExecute, false);
+        }
+
+        public void OnDeactivate(IObjAiBase owner, ISpell spell)
+        {
+        }
+
+        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
+        {
+        }
+
+        public void OnSpellCast(ISpell spell)
+        {
+        }
+
+        public void OnSpellPostCast(ISpell spell)
+        {
+        }
         public void TargetExecute(ISpell spell, IAttackableUnit target, ISpellMissile missile, ISpellSector sector)
         {
             var owner = spell.CastInfo.Owner;
             float ap = owner.Stats.AbilityPower.Total * 0.5f;
             float damage = 60 + (spell.CastInfo.SpellLevel - 1) * 30 + ap;
 
-            target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+            target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
 
         }
 
         public void OnSpellChannel(ISpell spell)
         {
-           
         }
 
         public void OnSpellChannelCancel(ISpell spell, ChannelingStopSource reason)
         {
-            
         }
-
         public void OnSpellPostChannel(ISpell spell)
         {
+
         }
 
         public void OnUpdate(float diff)
